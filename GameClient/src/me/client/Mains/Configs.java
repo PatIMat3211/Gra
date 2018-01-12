25b2b535179034865a43cd52fb13ea1e78b901d9
Msi <Msi@Msi-Komputer>
2018-01-12 18:33:44 +0100

    First Commit

---
+++ GameClient/src/me/client/Mains/Configs.java
@@ -0,0 +1,7 @@
+package me.client.Mains;
+
+public class Configs {
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	public static final int FPS_CAP = 120;
+}
---
+++ GameClient/src/me/client/Mains/Main.java
@@ -0,0 +1,9 @@
+package me.client.Mains;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
