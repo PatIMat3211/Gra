a0b6ec63305049d747b08a9c96918f16e8ef085a
Msi <Msi@Msi-Komputer>
2018-01-12 19:00:36 +0100

    Display Initalization and Game Loop Added

--- GameClient/src/me/client/Mains/Main.java
+++ GameClient/src/me/client/Mains/Main.java
@@ -1,9 +1,18 @@
 package me.client.Mains;
 
+import me.client.Rendering.DisplayManager;
+import me.client.Utilities.MyDisplay;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		MyDisplay display = DisplayManager.initalizeDisplay("Game");
+		
+		while(!display.shouldClose()) {
+			display.update();
+		}
+		
+		display.close();
 	}
 
 }
